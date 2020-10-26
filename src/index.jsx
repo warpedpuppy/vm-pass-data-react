38c2bcd4d5629dde221f9f33d3458a327956538f
Vidya Muniraju <vidya.muniraju@csu.fullerton.edu>
2020-10-25 21:44:32 -0500

    Adding initial structure

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+class MyFlixApplication extends React.Component {
+  render() {
+    return (
+      <div className="my-flix">
+        <div>Good morning</div>
+      </div>
+    );
+  }
+}
+
+// finds the root of the app
+const container = document.getElementsByClassName('app-container')[0];
+
+// tells react to render the app in the root DOM element
+ReactDOM.render(React.createElement(MyFlixApplication), container);
